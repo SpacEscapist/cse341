f4e32034115051387ba6a3336698a76786851e53
Branden Torrance <btorrance86@gmail.com>
2022-09-23 06:14:57 -0400

    Changes

--- node/app.js
+++ node/app.js
@@ -2,20 +2,21 @@ const express = require("express");
 const port = 3000;
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const indexRoute = require("./src/routes/index");
 const app = express();
 
-require("dotenv").config();
-
-const indexRoute = require("./src/routes/index");
+require("dotenv/config");
 
 // Middleware
 app.use(bodyParser.json());
 app.use("/", indexRoute);
 
 // Connect to database
-mongoose.connect(process.env.DB_CONNECTION);
+mongoose.connect(process.env.DB_CONNECTION, () => {
+  console.log("Connected to MongoDB");
+});
 
 // Bind the connection on a specific host and port
 app.listen(port, () => {
-  console.log(`Database connected and server is listening on port:${port}`);
+  console.log(`Server is listening on port:${port}`);
 });
